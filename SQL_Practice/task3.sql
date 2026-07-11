5667396e5f44bb66044d79e3b80e67e560dbc91d
Gudluri Vinitha <gudlurivinitha@gmail.com>
2026-07-11 18:30:25 +0530

    my task3

---
+++ SQL_Practice/task3.sql
@@ -0,0 +1,1 @@
+INSERT INTO student(student_id,student_name,age,city) VALUES (101,'Rahul',21,'Hyderabad'),(102,'Anjali',20,'Chennai'),(103,'Ravi',22,'Banglore'),(104,'Priya',19,'Pune'),(105,'Kiran',23,'Mumbai');
